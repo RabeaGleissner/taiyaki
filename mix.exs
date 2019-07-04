e7d45467449bfc92854600434e458bc53ac7f357
Rabea Gleissner <rabeagleissner@gmail.com>
2019-07-04 22:02:28 +0800

    Add slack dependency and basic functions

--- mix.exs
+++ mix.exs
@@ -20,9 +20,6 @@ defmodule Taiyaki.MixProject do
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
-    [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-    ]
+    [{:slack, github: "BlakeWilliams/Elixir-Slack", ref: "4812cf8"}]
   end
 end
